b98f641964a99933af8b283d21833cf46b649476
Seif <seif@jinglepay.com>
2025-06-30 13:21:00 +0400

    fixes

--- src/constants/locale/pak/ur/savings.js
+++ src/constants/locale/pak/ur/savings.js
@@ -1,11 +1,11 @@
 export default {
-  trustedBy: ' صارفین',
+  trustedBy: ' دنیا بھر میں',
   userNum: '500,000+',
-  usersWorldWide: 'دنیا بھر میں',
+  usersWorldWide: 'صارفین',
   transactionNum: 'USD 2',
   transactionText: 'بلین سے زائد ٹرانزیکشنز',
   zeroFees: 'زیرو فیس',
-  genrated: 'صارفین کے لیےکی بچت',
+  genrated: 'گاہکوں کے لیے پیدا کی گئی بچت',
   amount: 'USD 5,000,000',
   subtitle: 'جِنگل پے مالیاتی خدمات تک رسائی کو سب کے لیے ممکن بنا رہا ہے۔',
 };
