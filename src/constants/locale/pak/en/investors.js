7a44ea9f55bf0e241dd8e0e3425cf72ba430ebe5
Seif <seif@jinglepay.com>
2025-07-16 20:20:13 +0400

    feat: add investors page localization for ARE and PAK regions

--- src/constants/locale/pak/en/investors.js
+++ src/constants/locale/pak/en/investors.js
@@ -3,7 +3,7 @@ export default {
   description:
     'Backed by leading financial institutions and top-tier international VCs from the US—investors behind some of the world’s most iconic fintech and tech companies—we are supported by those who recognize transformational potential early. Our partners bring deep expertise, global networks, and a shared vision to reshape the future of finance.',
   cta: 'Become an Investor',
-  extraDescriptionTitle: 'Join us',
+  extraDescriptionTitle: 'Join Us',
   extraDescription:
     'Join us in redefining digital finance. As a leading FinTech in the MENAP region, we’re accelerating financial inclusion through seamless payments, cross-border remittances, and next-gen digital banking. Powered by proprietary technology and a fast-scaling user base, we represent a high-growth opportunity at the forefront of the FinTech revolution.',
   boldExtraDescription: 'Invest in the future of finance—backed by vision, driven by impact.',
