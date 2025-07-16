a6194797d0f0108f2112ed600f718dd6f2d331fc
Seif <seif@jinglepay.com>
2025-07-16 19:41:49 +0400

    feat: add English locale strings for control section

--- src/constants/locale/are/en/control.js
+++ src/constants/locale/are/en/control.js
@@ -2,18 +2,20 @@ export default {
   heading: {
     prefix: 'Take',
     highlight: 'full control',
-    suffix: 'of your finances with Jingle Pay'
+    suffix: 'of your finances with Jingle Pay',
   },
   description: {
-    part1: 'Discover the future of banking with our innovative app—crafted specifically for those seeking affordable, transparent, and cutting-edge financial solutions.',
-    part2: 'Transform your financial journey with Jingle Pay! Experience the most effortless and rewarding way to manage your money – sign up today and unlock exclusive benefits!'
+    part1:
+      'Discover the future of banking with our innovative app—crafted specifically for those seeking affordable, transparent, and cutting-edge financial solutions.',
+    part2:
+      'Transform your financial journey with Jingle Pay! Experience the most effortless and rewarding way to manage your money – sign up today and unlock exclusive benefits!',
   },
   benefits: {
-    title: 'What\'s in it for you?',
+    title: "What's in it for you?",
     items: [
-      'Send money internationally or locally with <span class="font-bold">Zero Fee</span>',
+      'Send money internationally or locally with <span class="font-bold">Zero Fees</span>',
       '<span class="font-bold">Request and receive money</span> directly to your wallet',
-      '<span class="font-bold">Earn cashback</span> every time you use your <span class="font-bold">Platinum MasterCard</span>'
-    ]
-  }
+      '<span class="font-bold">Earn cashback</span> every time you use your <span class="font-bold">Platinum MasterCard</span>',
+    ],
+  },
 };
